d4af80f3ec508d07fbe59a3202bf4ce79ea428bf
tsu-nera <fox1022fox@gmail.com>
2025-11-08 09:12:30 +0900

    init

---
+++ BootTidal.hs
@@ -0,0 +1,12 @@
+:set -XOverloadedStrings
+
+import Sound.Tidal.Context
+
+tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
+
+let p = streamReplace tidal
+let hush = streamHush tidal
+let d1 = p 1
+let d2 = p 2
+let d3 = p 3
+let d4 = p 4
